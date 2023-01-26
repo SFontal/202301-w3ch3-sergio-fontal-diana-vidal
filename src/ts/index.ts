459481db7ff3a7df7b130b4fae61f9ee3d303345
Diana Vidal <dvidaal@gmail.com>
2023-01-26 19:57:49 +0100

    Add header component

---
+++ src/ts/index.ts
@@ -0,0 +1,5 @@
+import ContainerHeaderComponent from "./components/ContainerHeader/ContainerHeaderComponent";
+
+const mainTitle = "My Series";
+
+const header = new ContainerHeaderComponent(mainTitle, "main-header");
