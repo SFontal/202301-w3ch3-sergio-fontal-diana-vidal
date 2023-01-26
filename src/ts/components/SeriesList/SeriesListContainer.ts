3551a720bb71218d388d62d57810374241ca3355
Diana Vidal <dvidaal@gmail.com>
2023-01-26 21:04:08 +0100

    Add component series list and interface for header and series list

--- src/ts/components/SeriesList/SeriesListContainer.ts
+++ src/ts/components/SeriesList/SeriesListContainer.ts
@@ -1,22 +1,22 @@
 import type TitleStructure from "../TitleStructure/TitleStructure";
 
-class ContainerHeaderComponent implements TitleStructure {
+class SeriesListComponent implements TitleStructure {
+  title: string;
   parentElement: Element;
   element: Element;
-  title: string;
 
   constructor(title: string, className: string) {
     this.parentElement = document.querySelector(".container")!;
-    this.element = document.createElement("header");
-    this.element.className = className;
+    this.element = document.createElement("main");
     this.title = title;
+    this.element.className = className;
     this.render();
   }
 
   render() {
-    this.element.innerHTML = `<h1 class="main-title">${this.title}</h1>`;
+    this.element.innerHTML = `<h2 class="main-content__title">${this.title}</h2>`;
     this.parentElement.appendChild(this.element);
   }
 }
 
-export default ContainerHeaderComponent;
+export default SeriesListComponent;
